39e6e670bb79694892a1b10aabef84a86cb79c65
WuangHuy <vuongquanghuy.dev@gmail.com>
2026-08-04 14:09:55 +0700

    chore: bump version to 1.1.4

--- test/features/device_sync/sharing_settings_section_test.dart
+++ test/features/device_sync/sharing_settings_section_test.dart
@@ -31,7 +31,7 @@ class _FakeSharingService implements LocalSharingService {
     pendingItems: 0,
     isTrusted: false,
     isBlocked: false,
-    appVersion: '1.1.3',
+    appVersion: '1.1.4',
     protocolVersion: '1',
   );
 
