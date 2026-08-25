9b388c00bd7091ee8a8d4751dcb5a3fb125b9fb2
Vuong Quang Huy <huyvq.bachkhoa@gmail.com>
2026-08-25 11:22:08 +0700

    chore: bump version to 1.1.6 and add GitHub issue forms

--- lib/features/device_sync/services/mdns_tls_local_sharing_service.dart
+++ lib/features/device_sync/services/mdns_tls_local_sharing_service.dart
@@ -22,7 +22,7 @@ class MdnsTlsLocalSharingService implements LocalSharingService {
   MdnsTlsLocalSharingService({
     DeviceIdentityStore? identityStore,
     this.enableMdns = true,
-    this.appVersion = '1.1.5',
+    this.appVersion = '1.1.6',
     this.platformOverride,
     this.reconnectDelayOverride,
   }) : _identityStore =
